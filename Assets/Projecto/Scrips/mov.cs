9d3ca8d9048409c04f16ef7ef161b51e49c218fb
Monke <gerard.busca@enti.cat>
2024-10-25 17:40:02 +0200

    Movement and hacking
    A fusion between the last two features.

--- Assets/Projecto/Scrips/mov.cs
+++ Assets/Projecto/Scrips/mov.cs
@@ -13,7 +13,7 @@ public class mov : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.enabled = false;
     }
 
     // Update is called once per frame
