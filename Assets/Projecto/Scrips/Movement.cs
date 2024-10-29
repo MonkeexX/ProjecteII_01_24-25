e317aab2b6a09365d6c8fe333fa41461904756cd
Monke <gerard.busca@enti.cat>
2024-10-29 17:15:57 +0100

    Movement changed entirely

--- Assets/Projecto/Scrips/Movement.cs
+++ Assets/Projecto/Scrips/Movement.cs
@@ -4,38 +4,71 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Vector2 TargetPosition;
+    private float moveTime = 0.15f;
+    private Vector2 targetPosition;
     private float xInput, yInput;
+    private bool isMoving;
     void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
 
-        if (xInput != 0f || yInput != 0f)
+        if ((xInput != 0f || yInput != 0f) && !isMoving && Input.anyKeyDown)
         {
             CalculateTargetPosition();
+            if (CanMoveToTargetPosition())
+            {
+                StartCoroutine(Move());
+            }
         }
+    }
+
+    IEnumerator Move()
+    {
+        isMoving = true;
+        float timePassed = 0f;
+        Vector2 startPosition = transform.position;
+
+        while (timePassed < moveTime)
+        {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, timePassed / moveTime);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        isMoving = false;
     }
 
     private void CalculateTargetPosition()
     {
         if (xInput == 1f)
         {
-            TargetPosition = (Vector2)transform.position + Vector2.right;
+            targetPosition = (Vector2)transform.position + Vector2.right;
         }
         else if (xInput == -1f)
         {
-            TargetPosition = (Vector2)transform.position + Vector2.left;
+            targetPosition = (Vector2)transform.position + Vector2.left;
         }
         if (yInput == 1f)
         {
-            TargetPosition = (Vector2)transform.position + Vector2.up;
+            targetPosition = (Vector2)transform.position + Vector2.up;
         }
         else if (yInput == -1f)
         {
-            TargetPosition = (Vector2)transform.position + Vector2.down;
+            targetPosition = (Vector2)transform.position + Vector2.down;
         }
 
-        private void On
+
+    }
+
+    private bool CanMoveToTargetPosition()
+    {
+        return !Physics2D.OverlapCircle(targetPosition, 0.15f);
+    }
+
+    //Quitar esto si al final este codigo se queda, esto es para el debugging
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(targetPosition, 0.15f);
     }
 }
